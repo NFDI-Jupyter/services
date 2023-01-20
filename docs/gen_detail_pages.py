9f5fcc9b42df98666b1b214fec06d4fb5dcec1a7
seeland <anett.seeland@dfki.de>
2023-01-20 12:40:42 +0100

    Fix for build: use virtual file creation

--- docs/gen_detail_pages.py
+++ docs/gen_detail_pages.py
@@ -12,12 +12,11 @@ services_names = [os.path.splitext(os.path.basename(file_name))[0] for file_name
 # generate the pages
 for file_name, service_name in zip(files, services_names):
     if os.path.basename(file_name) in exclude:
-        print("ignored")
         continue
     with open(file_name, 'r') as fh:
         d = {"yaml_str": fh.read()}
-    target_name = os.path.join("docs", "details", service_name + ".md")
-    with open(target_name, "w") as fh:
+    target_name = os.path.join("details", service_name + ".md")
+    with mkdocs_gen_files.open(target_name, "w") as fh:
         fh.write("---\n")
         fh.write(d["yaml_str"])
         fh.write("---\n")
